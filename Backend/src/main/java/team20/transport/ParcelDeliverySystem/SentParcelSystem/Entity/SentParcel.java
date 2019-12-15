62ca3dccf6e7d004a29780041bb9f4dd1aca4fec
mrzack99s <chatdanai374@gmail.com>
2019-12-15 21:47:58 +0700

    cloes #44 - ทำการเพิ่มไฟล์ใน Folder backend

---
+++ Backend/src/main/java/team20/transport/ParcelDeliverySystem/SentParcelSystem/Entity/SentParcel.java
@@ -0,0 +1,42 @@
+﻿package team20.transport.ParcelDeliverySystem.SnetParcelSystem.Entity;
+
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+import team20.transport.ParcelDeliverySystem.Entity.Employee;
+import team20.transport.ParcelDeliverySystem.Entity.Station;
+import team20.transport.ParcelDeliverySystem.Entity.Status;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@Setter
+@Table(name="SENTPARCEL")
+public class Sentparcel {
+    @Id
+    @SequenceGenerator(name="SENTPARCEL_SEQ",sequenceName="SENTPARCEL_SEQ",initialValue = 1,allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="SENTPARCEL_SEQ")
+    @Column(name="SENTPARCEL_ID",unique = true, nullable = true)
+    private @NonNull Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = Employee.class)
+    @JoinColumn(name = "EMPOLYEE_ID", insertable = true)
+    @JsonManagedReference
+    private Employee createBy;
+
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = Status.class)
+    @JoinColumn(name = "STATUS_ID", insertable = true)
+    @JsonManagedReference
+    private Status onStatus;
+
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = Station.class)
+    @JoinColumn(name = "STATION_ID", insertable = true)
+    @JsonManagedReference
+    private Station atStation;
+//
+
+}
---
+++ Backend/src/main/java/team20/transport/ParcelDeliverySystem/ShippingStateSystem/Entity/ShippingState.java
@@ -0,0 +1,49 @@
+package team20.transport.ParcelDeliverySystem.ShippingStateSystem.Entity;
+
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+import team20.transport.ParcelDeliverySystem.Entity.Employee;
+import team20.transport.ParcelDeliverySystem.Entity.Station;
+import team20.transport.ParcelDeliverySystem.Entity.Status;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Getter
+@Setter
+@Table(name="SHIPPINGSTATE")
+public class ShippingState {
+    @Id
+    @SequenceGenerator(name="SHIPPINGSTATE_SEQ",sequenceName="SHIPPINGSTATE_SEQ",initialValue = 1,allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="SHIPPINGSTATE_SEQ")
+    @Column(name="SHIPPINGSTATE_ID",unique = true, nullable = true)
+    private @NonNull Long id;
+
+    @Column(name="SHIPPINGSTATE_TIMESTAMP", nullable = false)
+    private @NonNull Timestamp timestamp;
+
+
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = Employee.class)
+    @JoinColumn(name = "EMPOLYEE_ID", insertable = true)
+    @JsonManagedReference
+    private Employee createBy;
+
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = Status.class)
+    @JoinColumn(name = "STATUS_ID", insertable = true)
+    @JsonManagedReference
+    private Status onStatus;
+
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = Station.class)
+    @JoinColumn(name = "STATION_ID", insertable = true)
+    @JsonManagedReference
+    private Station atStation;
+
+
+    // รอรับ Package จากไพรัช ต้นเขียน ครับบบบบ
+
+
+}
