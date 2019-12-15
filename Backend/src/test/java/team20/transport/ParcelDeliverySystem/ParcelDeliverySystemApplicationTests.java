a39a2308b0be96110503c2110bc797961d2a0cf0
mrzack99s <chatdanai374@gmail.com>
2019-12-15 21:38:31 +0700

    close #44

---
+++ Backend/src/test/java/team20/transport/ParcelDeliverySystem/ParcelDeliverySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package team20.transport.ParcelDeliverySystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcelDeliverySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
