24eb7934b011b715fb147d2120d0a1cd62c78dc3
athithan23123 <athithan23123@gmail.com>
2019-12-16 22:17:06 +0700

    เพิ่ม Entity ระบบย่อย ระบบยกเลิกพัสดุ

--- Backend/src/main/java/team20/transport/ParcelDeliverySystem/CancelsentSystem/Entity/Cancelsent.java
+++ Backend/src/main/java/team20/transport/ParcelDeliverySystem/CancelsentSystem/Entity/Cancelsent.java
@@ -1,26 +1,42 @@
-package com.cpe.backend.cancelsend.entity;
+package team20.transport.ParcelDeliverySystem.CancelSent.Entity;
 
-import lombok.*;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+import team20.transport.ParcelDeliverySystem.Entity.Status;
 
 import javax.persistence.*;
-import java.util.Collection;
 
-@Data
 @Entity
-@NoArgsConstructor
-@Table(name="Cancelsend")
-public class Cancelsend {
+@Getter
+@Setter
 
-
-
+@Table(name="CANCELSENT")
+public class Cancelsent {
     @Id
-    @SequenceGenerator(name="Cancelsend_seq",sequenceName="cancelsend_seq")
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="cancelsend_seq")
-    @Column(name = "Cancel_ID", unique = true, nullable = true)
+    @SequenceGenerator(name="CANCELSENT_SEQ",sequenceName="CANCELSENT_SEQ")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="CANCELSENT_SEQ")
+    @Column(name = "CANCELSENT_ID", unique = true, nullable = true)
     private @NonNull Long id;
 
-    @Column(name="Cancel_Price")
+    @Column(name="CANCEL_PRICE")
     private @NonNull Long price;
 
-//ew
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = Status.class)
+    @JoinColumn(name = "STATUS_ID", insertable = true)
+    @JsonManagedReference
+    private Status onStatus;
+
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = Senttoback.class)
+    @JoinColumn(name = "SENTTOBACK_ID", insertable = true)
+    @JsonManagedReference
+    private Status onSenttoback;
+
+    @ManyToOne(fetch = FetchType.EAGER, targetEntity = Senttoback.class)
+    @JoinColumn(name = "SENTTOBACK_ID", insertable = true)
+    @JsonManagedReference
+    private Status onHowtoPay;
+
 }
