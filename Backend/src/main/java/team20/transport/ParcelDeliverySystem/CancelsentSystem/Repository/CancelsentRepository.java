3869fa56961485139bf68b50c6fe2dd59bb77a06
athithan23123 <athithan23123@gmail.com>
2020-01-04 20:52:12 +0700

    แก้ไข Controller ระบบย่อย ระบบยกเลิกพัสดุ

--- Backend/src/main/java/team20/transport/ParcelDeliverySystem/CancelsentSystem/Repository/CancelsentRepository.java
+++
@@ -1,9 +0,0 @@
-package team20.transport.ParcelDeliverySystem.CancelSentSystem.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import team20.transport.ParcelDeliverySystem.CancelSentSystem.Entity.Cancelsent;
-
-@RepositoryRestResource
-public interface CancelsenteRepository extends JpaRepository<Cancelsent, Long> {
-}
--- Backend/src/main/java/team20/transport/ParcelDeliverySystem/CancelsentSystem/Repository/SenttobackRepository.java
+++
@@ -1,9 +0,0 @@
-package team20.transport.ParcelDeliverySystem.CancelSentSystem.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import team20.transport.ParcelDeliverySystem.CancelSentSystem.Entity.Senttoback;
-
-@RepositoryRestResource
-public interface SenttobackRepository extends JpaRepository<Senttobabk, Long> {
-}
