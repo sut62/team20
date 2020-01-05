11b5e12346b696d35bd76a9e1556c7259931da5b
pattarasit <pattarasit0@gmail.com.com>
2020-01-05 21:29:38 +0700

    close #57 - เพิ่ม Repository ของระบบย่อยระบบการจัดการพัสดุ

--- Backend/src/main/java/team20/transport/ParcelDeliverySystem/SentParcelSystem/Repository/SentParcelRepository.java
+++ Backend/src/main/java/team20/transport/ParcelDeliverySystem/SentParcelSystem/Repository/SentParcelRepository.java
@@ -1,7 +1,14 @@
 package team20.transport.ParcelDeliverySystem.SentParcelSystem.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import team20.transport.ParcelDeliverySystem.PackagingSystem.Entity.Packaging;
 import team20.transport.ParcelDeliverySystem.SentParcelSystem.Entity.SentParcel;
 
+import java.util.Collection;
+
 public interface SentParcelRepository  extends JpaRepository<SentParcel, Long> {
+    @Query(value = "SELECT * FROM SENTPARCEL WHERE SENTPARCEL_ID = :sentparcelId",nativeQuery = true)
+    Collection<SentParcel> findBySentparcelId(@Param("sentparcelId") Long sentparcelId);
 }
