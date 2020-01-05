dfb16488b76cd87222ace61b02568d28dcd0c6d3
mrzack99s <chatdanai374@gmail.com>
2020-01-05 22:53:27 +0700

    ทำการเพิ่ม Entity ของ EmployeePosition และแก้ไข Employee entity

---
+++ Backend/src/main/java/team20/transport/ParcelDeliverySystem/Entity/EmployeePosition.java
@@ -0,0 +1,29 @@
+package team20.transport.ParcelDeliverySystem.Entity;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Getter
+@Setter
+@Table(name="EMPOLYEEPOSITION")
+public class EmployeePosition {
+    @Id
+    @SequenceGenerator(name = "EMPOLYEEPOSITION_SEQ", sequenceName = "EMPOLYEEPOSITION_SEQ", initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "EMPOLYEEPOSITION_SEQ")
+    @Column(name = "EMPOLYEEPOSITION_ID", unique = true, nullable = true)
+    private @NonNull Long id;
+
+    @Column(name = "EMPOLYEEPOSITION_NPOSITION", unique = true, nullable = true)
+    private @NonNull Long position;
+
+    @OneToMany(fetch = FetchType.LAZY, targetEntity = Employee.class)
+    @JoinColumn(name = "EMPOLYEE_ID")
+    @JsonBackReference
+    private Collection<Employee> haveShippingState;
+}
