f7818b8ab35faf0cd7be997ec317778a67a23e7f
athithan23123 <athithan23123@gmail.com>
2020-01-06 21:25:56 +0700

    เพิ่ม UI ระบบย่อย ระบบยกเลิกพัสดุ

--- Frontend/src/views/Cancelsent System/Cancelsent.vue
+++ Frontend/src/views/Cancelsent System/Cancelsent.vue
@@ -1,45 +1,33 @@
 <template>
-    <div>
-        <b-card no-body>
-            <b-card-header header-tag="nav">
+<div>
+    <b-card no-body>
+        <b-card-header header-tag="nav">
             <b-nav card-header tabs>
                 <b-nav-item active>ระบบยกเลิกพัสดุ</b-nav-item>
             </b-nav>
-            </b-card-header>
+        </b-card-header>
 
-            <b-card-body class="text-center">
+        <b-card-body class="text-center">
             <b-card-title>แบบฟอร์มยกเลิกพัสดุ</b-card-title>
 
             <hr>
-
             <b-row class="mt-4 mb-4">
                 <b-col cols="1"></b-col>
                 <b-col cols="5">
-                    
+
                     <label for="selectList">เลือกชื่อพนักงาน</label>
-                    <b-form-select
-                    v-model="this.Cancelstate.employeeId"
-                    :options="this.stationData"
-                    class="mb-3"
-                    value-field="id"
-                    text-field="name"
-                    disabled-field="notEnabled"
-                    id="selectList"
-                    ></b-form-select>
+                    <b-form-select v-model="this.Cancelstate.employeeId" :options="this.stationData" class="mb-3" value-field="id" text-field="name" disabled-field="notEnabled" id="selectList"></b-form-select>
 
                     <label for="input-with-list">กรอก Package ID</label>
-                    <b-form-input 
-                        list="input-list" 
-                        v-model="this.stationData.packageId" 
-                        id="input-with-list"></b-form-input>
-                    <b-button class="mt-2"  @click="this.Search" >ค้นหา</b-button>
+                    <b-form-input list="input-list" v-model="this.stationData.packageId" id="input-with-list"></b-form-input>
+                    <b-button class="mt-2" @click="this.Search">ค้นหา</b-button>
                 </b-col>
                 <b-col cols="1"></b-col>
                 <b-col>
                     <b>สถานะการค้นหา Package</b>
                     <div class="text-left mt-2 text-break">
                         <div>
-                            สถานะ : 
+                            สถานะ :
                             <span v-if="this.haveSearch">
                                 <div v-if="this.foundPackage" class="badge badge-success text-wrap" style="width: 6rem;">
                                     พบ Package
@@ -60,14 +48,14 @@
                         <div v-if="this.foundPackage">
                             <hr>
                             <div class="ml-2">
-                               <p class="text-center mb-1"><b>ข้อมูล Package</b></p>
-                               ชื่อผู้ส่ง : {{this.packageData.srcName}} <br>
-                               ชื่อผู้รับ : {{this.packageData.dstName}} <br>
-                               ต้นสถานี : {{this.packageData.srcStation}} <br>
-                               ปลายทางสถานี : {{this.packageData.dstStation}} <br>
+                                <p class="text-center mb-1"><b>ข้อมูล Package</b></p>
+                                ชื่อผู้ส่ง : {{this.packageData.srcName}} <br>
+                                ชื่อผู้รับ : {{this.packageData.dstName}} <br>
+                                ต้นสถานี : {{this.packageData.srcStation}} <br>
+                                ปลายทางสถานี : {{this.packageData.dstStation}} <br>
                             </div>
                         </div>
-                        
+
                     </div>
                 </b-col>
                 <b-col cols="1"></b-col>
@@ -78,21 +66,11 @@
             <b-row class="mt-4 mb-4" v-if="this.foundPackage">
                 <b-col cols="1"></b-col>
                 <b-col>
-                <label for="selectList">เลือกสถานะ</label>
-                    <b-form-select
-                    v-model="this.Cancelstate.statusId"
-                    :options="this.statusData"
-                    class="mb-3"
-                    value-field="id"
-                    text-field="name"
-                    disabled-field="notEnabled"
-                    id="selectList"
-                    ></b-form-select>
+                    <label for="selectList">เลือกสถานะ</label>
+                    <b-form-select v-model="this.Cancelstate.statusId" :options="this.statusData" class="mb-3" value-field="id" text-field="name" disabled-field="notEnabled" id="selectList"></b-form-select>
                 </b-col>
                 <b-col cols="1"></b-col>
             </b-row>
-
-            
 
             <div v-if="this.foundPackage">
                 <hr>
@@ -100,17 +78,18 @@
                 <b-button variant="primary" @click="this.Save">บันทึก</b-button>
 
             </div>
-            </b-card-body>
-        </b-card>
-    </div>
+        </b-card-body>
+    </b-card>
+</div>
 </template>
+
 <script>
 export default {
     data() {
         return {
             foundPackage: false,
             haveSearch: false,
-            Cancelstate:{
+            Cancelstate: {
                 packageId: null,
                 employeeId: null,
                 statusId: null,
@@ -121,8 +100,7 @@ export default {
                 srcStation: "sT",
                 dstStation: "dT"
             },
-            employeeData:[
-                {
+            employeeData: [{
                     id: 1,
                     name: "test test"
                 },
@@ -131,34 +109,32 @@ export default {
                     name: "test2 test2"
                 }
             ],
-            stationData:[
-                {
-                    id:1,
+            stationData: [{
+                    id: 1,
                     name: "Station test1",
                 },
                 {
-                    id:2,
+                    id: 2,
                     name: "Station test2",
                 },
             ],
-            statusData:[
-                {
-                    id:1,
+            statusData: [{
+                    id: 1,
                     name: "Status test1",
                 },
                 {
-                    id:2,
+                    id: 2,
                     name: "Status test2",
                 },
             ]
         }
     },
-    methods:{
-        Search(){
+    methods: {
+        Search() {
             this.foundPackage = true
             this.haveSearch = true
         },
-        Save(){
+        Save() {
             alert("บันทึกสถานะพัสดุสำเร็จ")
         }
     }
