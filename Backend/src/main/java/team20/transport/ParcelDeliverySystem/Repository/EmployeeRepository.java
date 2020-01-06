3614a64a818d46cc6bb015f8a2850e020b3dfa59
mrzack99s <chatdanai374@gmail.com>
2020-01-06 19:24:08 +0700

    ทำการแก้ไข Employee controller และ Employee repository - #56

--- Backend/src/main/java/team20/transport/ParcelDeliverySystem/Repository/EmployeeRepository.java
+++ Backend/src/main/java/team20/transport/ParcelDeliverySystem/Repository/EmployeeRepository.java
@@ -1,9 +1,13 @@
 package team20.transport.ParcelDeliverySystem.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import team20.transport.ParcelDeliverySystem.Entity.Employee;
 
 @RepositoryRestResource
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    @Query(value = "SELECT * FROM EMPOLYEE WHERE EMPOLYEE_ID = :employeeId AND EMPOLYEE_EMAIL = :email",nativeQuery = true)
+    Employee findByIdAndEmail(@Param("employeeId") Long employeeId, @Param("email") String email);
 }
