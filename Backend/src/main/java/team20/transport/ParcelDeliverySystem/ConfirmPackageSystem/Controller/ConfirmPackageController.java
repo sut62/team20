2d8e505a700adf5c5161ef2813f9b96f8ff823b6
RyujiStudi0 <Riw.Kittitorn@gmail.com>
2020-01-24 00:50:19 +0700

    ทำการแก้ไข Controller ระบบย่อย ระบบยืนยันการรับพัสดุ

--- Backend/src/main/java/team20/transport/ParcelDeliverySystem/ConfirmPackageSystem/Controller/ConfirmPackageController.java
+++ Backend/src/main/java/team20/transport/ParcelDeliverySystem/ConfirmPackageSystem/Controller/ConfirmPackageController.java
@@ -1,6 +1,8 @@
 package team20.transport.ParcelDeliverySystem.ConfirmPackageSystem.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import team20.transport.ParcelDeliverySystem.Entity.Employee;
 import team20.transport.ParcelDeliverySystem.PackagingSystem.Entity.Packaging;
@@ -28,25 +30,30 @@ public class ConfirmPackageController {
     PackagingRepository packagingRepository;
 
     @PostMapping("/addConfirmPackage")
-    public ConfirmPackage addConfirmPackage(@RequestBody Map<String,Long> allParams){
+    @ResponseBody
+    public Object addConfirmPackage(@RequestBody Map<String,Long> allParams) {
 
         Packaging packaging = packagingRepository.findById(allParams.get("packageId")).get();
         Employee createBy = employeeRepository.findById(allParams.get("employeeId")).get();
         SatisfactionLevel satisfactionLevel = satisfactionLevelRepository.findById(allParams.get("satisfactionLevelId")).get();
 
-	Long countaddConfirmPackge = confirmPackageRepository.count();
+        if (packaging.getConfirmPackage() != null) {
+            return new ResponseEntity(HttpStatus.NOT_ACCEPTABLE);
+        }
 
-        String code = String.format("CPT20%05d",countaddConfirmPackge + 1);
+        long countaddConfirmPackge = confirmPackageRepository.count();
+        String code = String.format("CPT20%05d", countaddConfirmPackge + 1);
+        
         String name = "T20";
 
         ConfirmPackage newConfirmPackage = new ConfirmPackage();
-	newConfirmPackage.setName(name);
+        newConfirmPackage.setName(name);
         newConfirmPackage.setCode(code);
         newConfirmPackage.setConfirmDate(new Date());
         newConfirmPackage.setSatisfactionLevel(satisfactionLevel);
         newConfirmPackage.setCreateBy(createBy);
         newConfirmPackage.setPackaging(packaging);
 
-        return confirmPackageRepository.save(newConfirmPackage);
+            return confirmPackageRepository.save(newConfirmPackage);
     }
 }
