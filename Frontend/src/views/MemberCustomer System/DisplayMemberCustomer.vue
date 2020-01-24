adb144bc6fa2cf5452db7e134281eae5f5251beb
pream154 <pream_thamai@hotmail.com>
2020-01-24 22:45:54 +0700

    ทำการแก้ไขUI ระบบแสดงสมาชิกให้ลูกค้า #91

--- Frontend/src/views/MemberCustomer System/DisplayMemberCustomer.vue
+++ Frontend/src/views/MemberCustomer System/DisplayMemberCustomer.vue
@@ -9,10 +9,28 @@
 
         <b-card-body class="text-center">
             <b-card-title>ข้อมูลสมาชิก</b-card-title>
+            <div v-if="this.FO">
 
+                <div v-if="this.checkin" class="badge badge-success text-wrap">
+                    ยืนยันสถานะสมบูรณ์
+                </div>
+                <div v-else class="badge badge-danger text-wrap">
+                    ยื่นยันระบบล้มเหลว กรุณายืนยันสถานะพนักงาน {{ this.employeeData.name }}
+                </div>
+            </div>
+            <div v-else class="badge badge-primary">
+                กรุณายืนยันสถานะพนักงาน {{ this.employeeData.name }}
+            </div>
 
-            <b-table striped hover :items="this.MemberCustomer"></b-table>
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Email address" v-model="email" required="required">
+            </div>
+            <b-button variant="primary" @click="this.chk">ยืนยันการรับข้อมูล</b-button>
+
         </b-card-body>
+        <div v-if="this.checkin">
+            <b-table striped hover :items="this.MemberCustomer"></b-table>
+        </div>
 
     </b-card>
 
@@ -24,11 +42,26 @@ import api from "../../apiConnector"
 export default {
     data() {
         return {
-            MemberCustomer: []
+            employeeData: JSON.parse(localStorage.getItem("employeeLogin")),
+            email: null,
+            checkin: false,
+            MemberCustomer: [],
+            chking: "",
+            failcase: 0,
+            FO: false
 
         }
     },
     methods: {
+        chk() {
+            if (this.chking == this.email) {
+                this.checkin = true;
+            } else {
+
+                this.checkin = false;
+            }
+            this.FO = true;
+        },
         getMemberCustomer() {
             api.get("/getMemberCustomer")
                 .then(response => {
@@ -49,15 +82,19 @@ export default {
                     this.MemberCustomer = dict
                 })
         },
-    sourceIndex: function (arr) {
-        return arr.slice().sort(function (a, b) {
-            return a.id - b.id;
-        });
-    }
+        sourceIndex: function (arr) {
+            return arr.slice().sort(function (a, b) {
+                return a.id - b.id;
+            });
+        },
+        init() {
+            this.chking = this.employeeData.email;
+        }
     },
 
     mounted() {
         this.getMemberCustomer();
+        this.init();
     }
 }
 </script>
