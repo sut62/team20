b6539fc9c05c0c1fe003656024b5f05dcf9689f9
athithan23123 <athithan23123@gmail.com>
2020-01-24 16:32:50 +0700

    #89 - แก้ Controller ระบบย่อย ระบบแสดงประวัติยกเลิกพัสดุ

--- Backend/src/main/java/team20/transport/ParcelDeliverySystem/CancelsentSystem/Controller/DisplayCancelsentController.java
+++ Backend/src/main/java/team20/transport/ParcelDeliverySystem/CancelsentSystem/Controller/DisplayCancelsentController.java
@@ -2,32 +2,39 @@ package team20.transport.ParcelDeliverySystem.CancelsentSystem.Controller;
 
 import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
+
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import team20.transport.ParcelDeliverySystem.CancelsentSystem.Entity.Cancelsent;
 import team20.transport.ParcelDeliverySystem.CancelsentSystem.Repository.CancelsentRepository;
+import team20.transport.ParcelDeliverySystem.PackagingSystem.Entity.Packaging;
+import team20.transport.ParcelDeliverySystem.PackagingSystem.Repository.PackagingRepository;
 
 @RestController
-@RequestMapping("/team20")
+@RequestMapping("/team20 ") 
 @CrossOrigin(origins = {"*"})
 public class DisplayCancelsentController {
 
 	@Autowired
-    CancelsentRepository cancelSentRepository;
-
+	CancelsentRepository cancelSentRepository;
+	@Autowired
+	PackagingRepository packagingRepository;
     @GetMapping("/getCancelsent")
-    public JSONArray getMemberCustomer(){
-    	JSONArray allCancel = new JSONArray();
-    	for(Cancelsent x : cancelSentRepository.findAll()){
-    		JSONObject cancel = new JSONObject();
+    public JSONArray getCancelsent(@RequestBody Map<String,String> allParams){
+		JSONArray allCancel = new JSONArray();
+		for(Cancelsent x : cancelSentRepository.findAll()){
+			Packaging id = packagingRepository.findById(Long.valueOf(allParams.get(x))).get();
+			JSONObject cancel = new JSONObject();
     		cancel.put("id",x.getId());
 			cancel.put("Name",x.getName());
 			cancel.put("OnPackageing",x.getOnPackageing());
-    		cancel.put("Comment",x.getComment());
-    		cancel.put("CreateBy",x.getCreateBy());
+			cancel.put("CreateBy",x.getCreateBy());
     		cancel.put("OnSenttoback",x.getOnSenttoback());
 			cancel.put("OnHowtopay",x.getOnHowtopay());
-			cancel.put("OnStatus",x.getOnStatus());
+			cancel.put("price",(id.getWeight()*id.getVolume()*id.getSendingType().getUnit())*0.5);
+			cancel.put("Comment",x.getComment());
     		allCancel.add(cancel);
     	}
     	return allCancel;
