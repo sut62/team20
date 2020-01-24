66854e4aa0375448c01f368aaa9c43bf28c76fa1
athithan23123 <athithan23123@gmail.com>
2020-01-24 21:50:36 +0700

    #88 - แก้ไข DisplayController ของCancelsent

--- Backend/src/main/java/team20/transport/ParcelDeliverySystem/CancelsentSystem/Controller/DisplayCancelsentController.java
+++ Backend/src/main/java/team20/transport/ParcelDeliverySystem/CancelsentSystem/Controller/DisplayCancelsentController.java
@@ -9,8 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import team20.transport.ParcelDeliverySystem.CancelsentSystem.Entity.Cancelsent;
 import team20.transport.ParcelDeliverySystem.CancelsentSystem.Repository.CancelsentRepository;
-import team20.transport.ParcelDeliverySystem.PackagingSystem.Entity.Packaging;
-import team20.transport.ParcelDeliverySystem.PackagingSystem.Repository.PackagingRepository;
 
 @RestController
 @RequestMapping("/team20 ") 
@@ -19,13 +17,10 @@ public class DisplayCancelsentController {
 
 	@Autowired
 	CancelsentRepository cancelSentRepository;
-	@Autowired
-	PackagingRepository packagingRepository;
     @GetMapping("/getCancelsent")
-    public JSONArray getCancelsent(@RequestBody Map<String,String> allParams){
+    public JSONArray getCancelsent(){
 		JSONArray allCancel = new JSONArray();
 		for(Cancelsent x : cancelSentRepository.findAll()){
-			Packaging id = packagingRepository.findById(Long.valueOf(allParams.get(x))).get();
 			JSONObject cancel = new JSONObject();
     		cancel.put("id",x.getId());
 			cancel.put("Name",x.getName());
@@ -33,7 +28,7 @@ public class DisplayCancelsentController {
 			cancel.put("CreateBy",x.getCreateBy());
     		cancel.put("OnSenttoback",x.getOnSenttoback());
 			cancel.put("OnHowtopay",x.getOnHowtopay());
-			cancel.put("price",(id.getWeight()*id.getVolume()*id.getSendingType().getUnit())*0.5);
+			cancel.put("price",(x.getWeight()*x.getVolume()*x.getSendingType().getUnit())*0.5);
 			cancel.put("Comment",x.getComment());
     		allCancel.add(cancel);
     	}
